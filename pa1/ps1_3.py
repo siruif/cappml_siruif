7f245959e9e1ec7b7f8cd63dc891eb756cd4fb0f
Sirui Feng <siruif@uchicago.edu>
2016-04-02 22:28:39 -0500

    mod df index

--- pa1/ps1_3.py
+++ pa1/ps1_3.py
@@ -19,7 +19,7 @@ def fill_with_mean(df, input_data, output_filename, variables):
 		means = df[var].mean()
 		df[var] = df[var].fillna(means)
 	df = df.round({'Age':0, 'GPA':0, 'Days_missed':0})
-	df.to_csv(output_filename, mode = 'w')
+	df.to_csv(output_filename, mode = 'w', index_label = 'ID')
 
 def fill_with_conditional_mean(df, input_data, output_filename, conditions, \
 	variables):
@@ -30,7 +30,7 @@ def fill_with_conditional_mean(df, input_data, output_filename, conditions, \
 		df[var] = df[var].fillna(means)
 		df = df.reset_index()
 	df = df.round({'Age':0, 'GPA':0, 'Days_missed':0})
-	df.to_csv(output_filename, mode = 'w')
+	df.to_csv(output_filename, mode = 'w', index_label = 'ID')
 
 def fill_in_missing_values(input_data):
 
@@ -39,15 +39,15 @@ def fill_in_missing_values(input_data):
 
 	# Question 3 Part A
 	# Fill in missing values with the mean of the values for that attribute.
-	# fill_with_mean(df, input_data,'output/mock_student_data_mean.csv', \
-	# 	variables)
+	fill_with_mean(df, input_data,'output/mock_student_data_mean.csv', \
+		variables)
 
 	# Question 3 Part B
 	# Fill in missing values with a class-conditional mean(where the class
 		# is whether graduated or not).
-	# fill_with_conditional_mean(df, input_data, \
-	# 	'output/mock_student_data_conditional_mean.csv', ['Graduated'], \
-	# 	variables)
+	fill_with_conditional_mean(df, input_data, \
+		'output/mock_student_data_conditional_mean.csv', ['Graduated'], \
+		variables)
 
 	# Question 3 Part B
 	# A better approach might be filling in values with classes-conditional
