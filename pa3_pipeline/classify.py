97c6a02bad9ea2d6f4670bbfdc44c3de75767441
Sirui Feng <siruif@cs.uchicago.edu>
2016-05-06 13:59:20 -0500

    pl

--- pa3_pipeline/classify.py
+++ pa3_pipeline/classify.py
@@ -36,23 +36,8 @@ import time
 
 def define_clfs_params():
 
-    # clfs = {'RF': RandomForestClassifier(n_estimators=50, n_jobs=-1),
-    #     'ET': ExtraTreesClassifier(n_estimators=10, n_jobs=-1, \
-    #         criterion='entropy'),
-    #     'AB': AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), \
-    #         algorithm="SAMME", n_estimators=200),
-    #     'LR': LogisticRegression(penalty='l1', C=1e5),
-    #     'SVM': svm.SVC(kernel='linear', probability=True, random_state=0),
-    #     'GB': GradientBoostingClassifier(learning_rate=0.05, subsample=0.5, \
-    #         max_depth=6, n_estimators=10),
-    #     'NB': GaussianNB(),
-    #     'DT': DecisionTreeClassifier(),
-    #     'SGD': SGDClassifier(loss="hinge", penalty="l2"),
-    #     'KNN': KNeighborsClassifier(n_neighbors=3) 
-    #         }
 
     clfs = {'RF': RandomForestClassifier(n_estimators=50, n_jobs=-1),
-        
         
         'LR': LogisticRegression(penalty='l1', C=1e5),
         'GB': GradientBoostingClassifier(learning_rate=0.05, subsample=0.5, \
@@ -64,20 +49,13 @@ def define_clfs_params():
             }
 
     grid = { 
-    'RF':{'n_estimators': [1,10,100], \
-    'max_depth': [1,5,10,20,50], \
-    'max_features': ['sqrt','log2'], \
-    'min_samples_split': [2,5,10]},
+    'RF':{'n_estimators': [1,10,100], 'max_depth': [1,5,10,20,50], 'max_features': ['sqrt','log2'], 'min_samples_split': [2,5,10]},
     'LR': { 'penalty': ['l1','l2'], 'C': [0.00001,0.0001,0.001,0.01,0.1,1]},
-    'GB': {'n_estimators': [1,10,100,1000,10000], \
-    'learning_rate' : [0.001,0.01,0.05,0.1,0.5], \
-    'subsample' : [0.1,0.5,1.0], 'max_depth': [1,3,5,10,20,50,100]},
+    'GB': {'n_estimators': [1,10,100,1000,10000], 'learning_rate' : [0.001,0.01,0.05,0.1,0.5], 'subsample' : [0.1,0.5,1.0], 'max_depth': [1,3,5,10,20,50,100]},
     'NB' : {},
-    'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100], \
-    'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
+    'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
     'SVM' :{'C' :[0.00001,0.0001,0.001,0.01,0.1,1],'penalty':['l1','l2']},
-    'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'], \
-    'algorithm': ['auto','ball_tree','kd_tree']}
+    'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'], 'algorithm': ['auto','ball_tree','kd_tree']}
            }
 
     return clfs, grid
@@ -192,7 +170,7 @@ def output_clf_log(clf_log):
 
 def main(filename): 
     clfs,grid = define_clfs_params()
-    models_to_run=['KNN','RF','LR','ET','AB','GB','NB','DT']
+    models_to_run=['KNN','RF','LR','GB','NB','DT']
 
     #get X and y
     df = pd.read_csv(filename, index_col = 0)
